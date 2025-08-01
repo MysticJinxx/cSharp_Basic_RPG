0152a9890a381303319fa1b5016c0ff67f250d40
Joshua Dishong <dishong.joshua@yahoo.com>
2025-07-31 21:46:27 -0700

    added new properties as lists

--- Projects/Engine/Monster.cs
+++ Projects/Engine/Monster.cs
@@ -13,6 +13,7 @@ namespace Engine
         public int MaximumDamage { get; set; }
         public int RewardExperiencePoints { get; set; }
         public int RewardGold { get; set; }
+        public List<LootItem> LootTable { get; set; }
 
         // Class constructor for Monster inheriting from LivingCreature constructor //
         public Monster(int id, string name, int maximumDagame, int rewardExperiencePoints, int rewardGold, int currentHitPoints, int maximumHitPoints) : base(currentHitPoints, maximumHitPoints)
@@ -22,6 +23,7 @@ namespace Engine
             MaximumDamage = maximumDagame;
             RewardExperiencePoints = rewardExperiencePoints;
             RewardGold = rewardGold;
+            LootTable = new List<LootItem>();
         }
     }
 }
